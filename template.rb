381ad2bc10fb4bab94b502b2681b91069ed99d50
shigeru10 <shiiiiigeeeee10@gmail.com>
2018-03-28 13:21:57 +0900

    output csv

--- template.rb
+++ template.rb
@@ -1,16 +1,23 @@
 require 'mechanize'
+require 'csv'
 
 agent = Mechanize.new
 base_url = 'https://qiita.com'
 page = agent.get(base_url + '/tags/Ruby/items')
 elements = page.search('a.u-link-no-underline')
 
-elements.each do |element|
-  path = element.get_attribute('href')
-  url = base_url + path
-  title = element.inner_text
+CSV.open('sample.csv', 'w') do |csv|
+  csv << %w[title url]
 
-  p 'title: ' + title
-  p 'url' + url
-  p '---------------------------------'
+  elements.each do |element|
+    path = element.get_attribute('href')
+    url = base_url + path
+    title = element.inner_text
+
+    csv << [title, url]
+
+    p 'title: ' + title
+    p 'url' + url
+    p '---------------------------------'
+  end
 end
